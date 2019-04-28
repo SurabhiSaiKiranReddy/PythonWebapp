8beaabd30389ecedf05a4a289ab12babff100ea5
bhavishyaareddy <bhavishyareddy265@gmail.com>
2019-04-28 14:45:56 -0500

    finnnal

--- server.py
+++ server.py
@@ -432,4 +432,4 @@ if __name__ == '__main__':
 
     app.secret_key='secret123'
     # app.run(threaded=True)
-    app.run(host='0.0.0.0',posrt=80,threaded=True)
+    app.run(host='0.0.0.0',port=80,threaded=True)
